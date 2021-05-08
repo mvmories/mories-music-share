a93b371e68089ab25e83d8eb49566edc0e102e74
mvmories <mvmories@gmail.com>
2021-05-08 15:19:33 +0100

    Updated the song playing functionality, queue previous and next song functionality attached to buttons and automatic behavior development, added localhost features through apollo client gql

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -13,6 +13,7 @@ import React from "react";
 import { SongContext } from "../App";
 import { GET_QUEUED_SONGS } from "../graphql/queries";
 import QueuedSongList from "./QueuedSongList";
+import ReactPlayer from "react-player";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -44,11 +45,60 @@ const useStyles = makeStyles((theme) => ({
 
 function SongPlayer() {
   const { data } = useQuery(GET_QUEUED_SONGS);
+  const reactPlayerRef = React.useRef();
   const { state, dispatch } = React.useContext(SongContext);
+  const [played, setPlayed] = React.useState(0);
+  const [playedSeconds, setPlayedSeconds] = React.useState(0);
+  const [seeking, setSeeking] = React.useState(false);
+  const [positionInQueue, setPositionInQueue] = React.useState(0);
   const classes = useStyles();
+
+  React.useEffect(() => {
+    const songIndex = data.queue.findIndex((song) => song.id === state.song.id);
+    setPositionInQueue(songIndex);
+  }, [state.song.id, data.queue]);
+
+  React.useEffect(() => {
+    const nextSong = data.queue[positionInQueue + 1];
+    if (played >= 0.99 && nextSong) {
+      setPlayed(0);
+      dispatch({ type: "SET_SONG", payload: { song: nextSong } });
+    }
+  }, [data.queue, played, positionInQueue, dispatch]);
 
   function handleTogglePlay() {
     dispatch(state.isPlaying ? { type: "PAUSE_SONG" } : { type: "PLAY_SONG" });
+  }
+
+  function handleProgressChange(event, newValue) {
+    setPlayed(newValue);
+  }
+
+  function handleSeekMouseDown() {
+    setSeeking(true);
+  }
+
+  function handleSeekMouseUp() {
+    setSeeking(false);
+    reactPlayerRef.current.seekTo(played);
+  }
+
+  function formatDuration(seconds) {
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
+  }
+
+  function handlePlayPrevSong() {
+    const prevSong = data.queue[positionInQueue - 1];
+    if (prevSong) {
+      dispatch({ type: "SET_SONG", payload: { song: prevSong } });
+    }
+  }
+
+  function handlePlayNextSong() {
+    const nextSong = data.queue[positionInQueue + 1];
+    if (nextSong) {
+      dispatch({ type: "SET_SONG", payload: { song: nextSong } });
+    }
   }
 
   return (
@@ -64,7 +114,7 @@ function SongPlayer() {
             </Typography>
           </CardContent>
           <div className={classes.controls}>
-            <IconButton>
+            <IconButton onClick={handlePlayPrevSong}>
               <SkipPrevious />
             </IconButton>
             <IconButton onClick={handleTogglePlay}>
@@ -74,15 +124,36 @@ function SongPlayer() {
                 <PlayArrow className={classes.playIcon} />
               )}
             </IconButton>
-            <IconButton>
+            <IconButton onClick={handlePlayNextSong}>
               <SkipNext />
             </IconButton>
             <Typography variant="subtitle1" component="p" color="textSecondary">
-              00:13:30
+              {formatDuration(playedSeconds)}
             </Typography>
           </div>
-          <Slider type="range" min={0} max={1} step={0.01} />
+          <Slider
+            onMouseDown={handleSeekMouseDown}
+            onMouseUp={handleSeekMouseUp}
+            onChange={handleProgressChange}
+            value={played}
+            type="range"
+            min={0}
+            max={1}
+            step={0.01}
+          />
         </div>
+        <ReactPlayer
+          ref={reactPlayerRef}
+          onProgress={({ played, playedSeconds }) => {
+            if (!seeking) {
+              setPlayed(played);
+              setPlayedSeconds(playedSeconds);
+            }
+          }}
+          url={state.song.url}
+          playing={state.isPlaying}
+          hidden
+        />
         <CardMedia className={classes.thumbnail} image={state.song.thumbnail} />
       </Card>
       <QueuedSongList queue={data.queue} />
